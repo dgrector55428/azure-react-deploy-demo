fe9f9bf8e8f870d44e69afa7912fca617b33c501
darrenrector91 <darrenrector91@gmail.com>
2020-11-29 18:48:07 -0600

    added web.config file

--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../App.css";
 
 function Page1() {
   return (
     <div className="page page1">
       <div className="flagTop" />
-      <div className="flagCenter">
+      <div
+        className="flagCenter"
+        style={{ background: "darkgrey", color: "#000" }}
+      >
         <h1 className="page">PAGE 1</h1>
         <div className="otherLinks">
           <Link to="/page2">PAGE 2</Link>
